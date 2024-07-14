0c32eacdf39a96e0e359619f63aceee36309ee9b
Abanoub Nashaat <onepercentpepo@gmail.com>
2024-07-14 22:07:25 +0300

    Move Index.html to Root To Deploy

--- src/js/main.js
+++ src/js/main.js
@@ -27,12 +27,12 @@ $(() => {
   const navItems = $("nav .nav-content li");
 
   if (nav.hasClass("collapsed")) {
-    arrow.attr("src", "./images/icons/Arrow-Nav-Right.svg");
+    arrow.attr("src", "src/images/icons/Arrow-Nav-Right.svg");
     $("nav .arrow").addClass("right-[-40px]");
     nav.css({ left: "-250px" }); // Ensure the nav is positioned correctly
     navItems.hide();
   } else {
-    arrow.attr("src", "./images/icons/Arrow-Nav-Left.svg");
+    arrow.attr("src", "src/images/icons/Arrow-Nav-Left.svg");
     $("nav .arrow").addClass("right-[-20px]");
   }
 
@@ -42,13 +42,13 @@ $(() => {
     if (isCollapsed) {
       navItems.slideDown(700);
       nav.animate({ left: "0" }, 500, function () {
-        arrow.attr("src", "./images/icons/Arrow-Nav-Left.svg");
+        arrow.attr("src", "src/images/icons/Arrow-Nav-Left.svg");
         $("nav .arrow").removeClass("right-[-40px]").addClass("right-[-20px]");
       });
     } else {
       navItems.slideUp(500);
       nav.animate({ left: "-250px" }, 500, function () {
-        arrow.attr("src", "./images/icons/Arrow-Nav-Right.svg");
+        arrow.attr("src", "src/images/icons/Arrow-Nav-Right.svg");
         $("nav .arrow").removeClass("right-[-20px]").addClass("right-[-40px]");
       });
     }
