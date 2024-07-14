30839030ff1842417e769ca9ade3f53c8c7faa65
Abanoub Nashaat <onepercentpepo@gmail.com>
2024-07-14 05:33:28 +0300

    Design Side-Bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  daisyui: {
+    themes: ["dark"],
+  },
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: ["Inter", "sans-serif"],
+      },
+      colors: {
+        bg: "#1e1e1e",
+        nav: "rgba(0, 0, 0, 0.47)",
+        navContent: "#6F7173",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
